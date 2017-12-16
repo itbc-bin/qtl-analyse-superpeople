85ab26576d684710fd57d109b2f4785c69c4d2fb
grimcode <a.janse@student.han.nl>
2017-12-16 14:18:32 +0100

    alles werkt en de resultaten zijn nagekeken

--- doeDeAnova.R
+++ doeDeAnova.R
@@ -1,35 +1,92 @@
+# only if xlsx gives error
 Sys.setenv(JAVA_HOME="C:\\Program Files\\Java\\jdk1.8.0_131\\jre")
 install.packages("rJava")
 library(rJava)
 install.packages("xlsx")
 library(xlsx)
 
-abData <- read.csv("CvixLerC9Rready3.csv",sep=",",header=FALSE) # Data met de aanwezige markers ophalen
-abTable <- t(abData)                                            # Marker data opslaan in een tabel
-colnames(abTable) <- abTable[1,]    # eerste rij tot collom headers maken 
-abTable <- abTable[-1,]             # eerste rij verwijderen 
+abData <- read.csv("CvixLerC9Rready3.csv",sep=",",header=FALSE) # Retrieve data
+abTable <- t(abData)                                            # Put data in table
+colnames(abTable) <- abTable[1,]                                # Use first row to create colums names 
+abTable <- abTable[-1,]                                         # Remove first row
 
-fenoData <- read.csv("CvixLerC9.qua~",sep="\t",header=FALSE) # fenotypedata ophalen
-row.names(abTable) <- fenoData[,1]
-abTable <- cbind(abTable, fenoData[2])                     # deze data opslaan in de tabel
-colnames(abTable)[ncol(abTable)] <- "Fenotype"     #colommen een naam geven
-fenoData <- transform(abTable, Fenotype = as.numeric(levels(Fenotype))[Fenotype])
+phenoData <- read.csv("CvixLerC9.qua~",sep="\t",header=FALSE)   # Phenotypedata ophalen
+row.names(abTable) <- phenoData[,1]                             # Give rows the name of the plant
+abTable <- cbind(abTable, phenoData[2])                         # Put data in table
+colnames(abTable)[ncol(abTable)] <- "Phenotype"                 # Give the last column a name
+phenoData <- suppressWarnings(transform(abTable, Phenotype = as.numeric(levels(Phenotype))[Phenotype]))
+                                                                # Tell R that the data is numeric. If not: data is corrupted
+                                                                # The supress warning is the warning that if a letter is in the data it will be conferted to NA
 
-#View(abTable)
+abTable[abTable=="-"] <- NA                                     # Replace unavailable data with NA
 
-abTable[154,ncol(abTable)] <- NA # onbekende waarde vervangen door NA
-abTable[abTable=="-"] <- NA
-
-for(naam in colnames(abTable[1:ncol(abTable)-1])){
-  test <- abTable[,c(naam,"Fenotype")]
-  test <- na.exclude(test)
-  test <- suppressWarnings(transform(test, Fenotype = as.numeric(levels(Fenotype))[Fenotype]))
-  #View(test)
-  
-  attach(test)
-  
-  result <- aov(formula = test[,"Fenotype"]~test[,1])
-  print(summary(result)[[1]][["Pr(>F)"]][[1]])
-  detach(test)
+# Create function that put multple strings from vector in a single string
+getNames <- function(x,print=TRUE,npar=TRUE){
+  p <- NULL
+  for (plant in x){
+    if(length(p) == 0){
+      p <- plant
+    } 
+    else {
+      p <- paste(p,plant,sep=",")
+    }
+  }
+  return(p)
 }
 
+
+resultMatrix <- NULL                                          # Create empty variable to store the results
+for(naam in colnames(abTable[1:ncol(abTable)-1])){
+  # In this loop all the markers are isolated with the phenotype data, 
+  # used for test and after isolation of different variables stored in an xlsx file
+  markerTable <- abTable[,c(naam,"Phenotype")]
+  containNA <- getNames(row.names(markerTable[rowSums(is.na(markerTable)) > 0,]))
+
+  markerTable <- na.exclude(markerTable)
+  markerTable <- suppressWarnings(transform(markerTable, Phenotype = as.numeric(levels(Phenotype))[Phenotype]))
+  
+  marker <- colnames(markerTable[1])
+  result <- aov(formula = markerTable[,"Phenotype"]~markerTable[,1])
+  
+  pValue <- summary(result)[[1]][["Pr(>F)"]][[1]]
+  fValue <- summary(result)[[1]][["F value"]][[1]]
+  SSbetween <- summary(result)[[1]][["Sum Sq"]][[1]]
+  SSwithin <- summary(result)[[1]][["Sum Sq"]][[2]]
+  
+  A <- markerTable[markerTable[,1]=="a",]
+  B <- markerTable[markerTable[,1]=="b",]
+  
+  nPlantsA <- nrow(A)
+  nPlantsB <- nrow(B)
+  
+  varA <- var(A[,2])
+  varB <- var(B[,2])
+  meanA <- mean(A[,2])
+  meanB <- mean(B[,2])
+  
+  plantsA <- getNames(row.names(A))
+  plantsB <- getNames(row.names(B))
+  
+  aovAllowed <- (!(varA/varB > 5 || varA/varB < -5))
+  
+  # If else statement to either create and add the first data row 
+  # or add new data rows to the result matrix
+  if(is.null(resultMatrix)){
+    resultMatrix <- matrix(c(marker,aovAllowed,pValue,fValue,
+                             plantsA,nPlantsA,meanA,varA,
+                             plantsB,nPlantsB,meanB,varB,
+                             SSbetween,SSwithin,containNA),ncol = 15)
+    colnames(resultMatrix) <- c("Marker", "ANOVA allowed","ANOVA P-value",
+                                "F value","plants A","n A","mean A","var.A",
+                                "plants B", "n B", "mean B","var.B", 
+                                "SS between groups","SS within groups",
+                                "Excluded plants")
+  } else {
+    resultMatrix <- rbind(resultMatrix, 
+                        c(marker,aovAllowed,pValue,fValue,
+                          plantsA,nPlantsA,meanA,varA,
+                          plantsB,nPlantsB,meanB,varB,
+                          SSbetween,SSwithin,containNA))
+  }
+}
+write.xlsx(resultMatrix,"resultTable.xlsx",row.names = F)         # Save the result matrix in a xlsx file without rownumbers
